21900dbc8a1777cac4574cd1f4fbe833f5cc2876
CodingNeko <codingneko@gmail.com>
2019-04-07 17:00:27 +0200

    desktop update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/*
----                           ---
----     PAGINATION GARBAGE    ---
----                           ---
-*/
 var currentPage = 0;
+var searchTags = document.getElementById("search-tags").value;
+var postsPerPage = 15;
+
+
 function updatePager(){
   $(".pi1 a").text(currentPage+1);
   $(".pi2 a").text(currentPage+2);
@@ -14,7 +13,7 @@ $(".pip").click(function(event){
   event.preventDefault();
   if(currentPage > 0){
     currentPage--;
-    importImgs();
+    fetchImages(searchTags, currentPage, 25);
   }else{
     currentPage=0;
   }
@@ -29,80 +28,52 @@ $(".pi1").click(function(event){
 $(".pi2").click(function(event){
   event.preventDefault();
   currentPage++;
-  importImgs();
+  fetchImages(searchTags, currentPage, 25);
   updatePager();
 });
 
 $(".pi3").click(function(event){
   event.preventDefault();
   currentPage += 2;
-  importImgs();
+  fetchImages(searchTags, currentPage, 25);
   updatePager();
 });
 
 $(".pin").click(function(event){
   event.preventDefault();
   currentPage++;
-  importImgs();
+  fetchImages(searchTags, currentPage, 25);
   updatePager();
 });
 
-
-/*
----                           ---
----     IMAGE LOADING SHIT    ---
----                           ---
-*/
-
-function fixImg(img){
-  var file = img.src;
-  file = file.substr(0, file.indexOf("/", 8)) + "/" + file.substr(file.indexOf("/", 8));
-  img.src = file;
-  console.log("error loading img");
-  img.onerror = "";
-}
-
-function appendImg(val){
-  var file = val.file_url;
-  var isVid = (file.includes("mp4") || file.includes("webm") || file.includes("avi") || file.includes("mpg") || file.includes("mpeg"));
-  if(!isVid){
-    $(".results").append("<a href=\"" + file + "\" class=\"image\"><img src=\"" + file + "\" class=\"img\" onerror=\"fixImg(this)\"/></a>");
-  }else{
-    var thumb = "thumbnail_" + val.image.split(".")[0] + ".jpg";
-    var thumbURL = file.substr(0, file.lastIndexOf("/")).replace("images", "thumbnails") + "/" + thumb;
-    $(".results").append("<a href=\"" + file + "\" class=\"image\"><img src=\"" + thumbURL + "\" class=\"img video\"/></a>");
-  }
-}
-
-function importImgs(){
-  var tags = $(".search-tags").val();
-  var page = currentPage;
-  $(".results").empty();
-
-  $.ajax({
-    url: "http://codingneko-eval-test.apigee.net/gelbooru",
-    type: "GET",
-    data: {
-      json: 1,
-      q: "index",
-      s: "post",
-      page: "dapi",
-      tags: tags,
-      pid: page,
-      limit: 15
-    },
-    timeout: 2000,
-    success: function(result){
-      $.each(result, function(i, val){
-        appendImg(val);
+function fetchImages(tags, page, limit){
+  fetch("http://codingneko-eval-test.apigee.net/gelbooru?page=dapi&s=post&q=index&json=1&tags=rating:safe " + tags + "&pid=" + page + "&limit=" + limit).then(function(response){
+    return response.json();
+  }).then(function(data){
+    document.getElementById("results").innerHTML = "";
+    document.getElementById("search-tags").value = searchTags;
+    data.forEach(jsonImage => {
+      let image = new Image(jsonImage.hash, jsonImage.directory);
+      image.render(jsonImage.hash);
+      document.getElementById(jsonImage.hash).addEventListener("click", function(){
+        image.maximize(jsonImage.file_url);
       });
-    },
+    });
+  }).catch(error => {
+      document.getElementById("results").innerHTML = "No images were found ";
+      console.log();
   });
 }
 
-importImgs();
+document.getElementById("post-quantity").addEventListener("change", function(){
+  postsPerPage = this.value;
+  fetchImages(searchTags, currentPage, postsPerPage);
+});
 
-$(".search").click(function(){
-  currentPage = 0;
-  importImgs();
+fetchImages("", 0, postsPerPage);
+
+document.getElementById("search-form").addEventListener("submit", function(e){  
+  e.preventDefault();
+  searchTags = document.getElementById("search-tags").value;
+  fetchImages(searchTags, currentPage, postsPerPage);
 });
